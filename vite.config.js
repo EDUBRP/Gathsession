f9ebc4b8d26d92a8dc1326a826f13decdae740e8
Eduardo Barroso <edubarrosoparra@gmail.com>
2025-07-15 23:13:50 +0200

    cambiovite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./",
+  base: "/Gathsession/",
   server: {
     watch: {
       usePolling: true, // Usa polling para detectar cambios
